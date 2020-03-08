a9a9d4aa6221b9084a76f74c94be459bdf9e31d5
tienan.liu <tienan.liu@tienan.liu>
2020-03-08 20:54:26 +0800

    daily practice

--- cpp/0198.cpp
+++ cpp/0198.cpp
@@ -5,20 +5,25 @@ using namespace std;
 class Solution
 {
 public:
-    inline int helper(vector<int> &nums, int i){
+    inline int helper(vector<int> &nums, int i)
+    {
         return max(nums[i - 1], nums[i - 2] + nums[i]);
     }
+
     int rob(vector<int> &nums)
     {
         int n = nums.size();
-        if (n == 0){
+        if (n == 0)
+        {
             return 0;
         }
-        if (n == 1){
+        if (n == 1)
+        {
             return nums[0];
         }
         nums[1] = max(nums[0], nums[1]);
-        for (int i = 2; i < n; ++i){
+        for (int i = 2; i < n; ++i)
+        {
             nums[i] = helper(nums, i);
         }
         return nums[n - 1];
